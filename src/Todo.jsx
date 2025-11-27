8a704cd897d871e98b84302205961bbfdd4fa40d
Ashikmridha793 <ashikmridha793@gmail.com>
2025-11-27 17:32:52 +0600

    conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -32,10 +32,30 @@
 // }
 
 
-// conditional rendering option 1: 
-export default function Todo({task, isDone}){
+// conditional rendering option 4: &&
+// export default function Todo({task, isDone}){
 
-      return (
-            <li>Task: {task}</li>
-      )
+//       return (
+//             <li>{task} {isDone && ': Done'}</li>
+//       )
+// }
+
+// // conditional rendering option 5: ||
+// export default function Todo({task, isDone}){
+
+//       return (
+//             <li>{task} {isDone || ': Do it'}</li>
+//       )
+// }
+
+// conditional rendering option 6: 
+export default function Todo({task, isDone}){
+      let listItem;
+      if(isDone){
+            listItem = <li>Finished: {task}</li>
+      }
+      else{
+            listItem= <li>Work on: {task}</li>
+      }
+      return listItem;
 }
