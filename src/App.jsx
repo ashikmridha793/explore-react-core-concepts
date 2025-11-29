6f9f7b20603b132a682c297c1aa53a1154274e2b
Ashikmridha793 <ashikmridha793@gmail.com>
2025-11-29 21:06:51 +0600

    add singer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,53 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./todo";
-
+import Actor from "./Actor";
+import Singer from "./singer";
 function App() {
+
+  const actors = ['sakib', 'Shoriful, raj', 'josim', 'rubel', 'salman']
+  const singers = [
+    {id:1, name: 'dr. Mahfujar rohman', age: 68},
+    {id:2, name: 'Eva Rahman', age: 46},
+    {id:3, name: 'Shuvro Dev', age: 65},
+    {id:4, name: 'Pretom vai', age: 29},
+  ]
+
   return (
     <>
       <h1>Vite + React</h1>
-      <Todo task="Learn React" isDone={true}></Todo>
+
+
+      {
+        singers.map(singer => <Singer singer={singer}></Singer>)
+      }
+
+
+
+      <Actor name = {'Bappa Raj'}></Actor>
+      {
+        actors.map(actor => <Actor name={actor}></Actor>)
+      }
+
+
+
+     {/*
+      { <Todo task="Learn React" isDone={true}></Todo>
       <Todo task="Core concepts" isDone={false}></Todo>
-      <Todo task="Try JSX" isDone={true}></Todo>
-      {/* <Device name="Laptop" price="85000"></Device>
+      <Todo task="Try JSX" isDone={true}></Todo>} */
+      }
+
+      {/*
+      <Device name="Laptop" price="85000"></Device>
       <Device name="Mobile" price="15000"></Device>
       <Device name="Watch" price="500"></Device>
       <Person grade='8' score='84'></Person>
       <Student grade={12} score='98'></Student>
       <Student></Student>
       <Student></Student>
-      <Developer></Developer> */}
-    </>
+      <Developer></Developer> */
+      }
+      </>
   );
 }
 function Device(props) {
@@ -44,30 +74,30 @@ function Person() {
     </h3>
   );
 }
-const { grade, score } = { grade: "8", score: "84" };
-function Student(grade = 1, score = 0) {
-  console.log(props);
-  return (
-    <div className="stident">
-      <h3>This is a student</h3>
-      <p>Class: {grade}</p>
-      <p>Score: {score}</p>
-    </div>
-  );
-}
-function Developer() {
-  const developerStyle = {
-    margin: "20px",
-    padding: "20px",
-    border: "2px solid purple",
-    borderRadius: "10px",
-  };
-  return (
-    <div style={developerStyle}>
-      <h5>Debo debo</h5>
-      <p>Coding: </p>
-    </div>
-  );
-}
+// const { grade, score } = { grade: "8", score: "84" };
+// function Student(grade = 1, score = 0) {
+//   console.log(props);
+//   return (
+//     <div className="stident">
+//       <h3>This is a student</h3>
+//       <p>Class: {grade}</p>
+//       <p>Score: {score}</p>
+//     </div>
+//   );
+// }
+// function Developer() {
+//   const developerStyle = {
+//     margin: "20px",
+//     padding: "20px",
+//     border: "2px solid purple",
+//     borderRadius: "10px",
+//   };
+//   return (
+//     <div style={developerStyle}>
+//       <h5>Debo debo</h5>
+//       <p>Coding: </p>
+//     </div>
+//   );
+// }
 
 export default App;
